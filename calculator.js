d6fff3b3c2dbed25955dd7fd4094db974f4e034d
Saransh <saranshlingwal923@gmail.com>
2022-07-17 11:26:25 +0530

    new commit

--- calculator.js
+++ calculator.js
@@ -12,13 +12,26 @@ app.get('/', function(req , res){
 
 app.post('/' , function(req , res){
 
-var number1 = Number(req.body.num1);
-var number2 = Number(req.body.num2);
+var num1 = Number(req.body.num1);
+var num2 = Number(req.body.num2);
 
-var result = number1+number2;
+var result = num1 + num2;
 
-    res.send("The result of the calculation is " + result );
+    res.send("Sum is " + result );
+});
+
+
+app.get("/bmicalculator" , function(req , res){
+    res.sendFile(__dirname + "/bmiCalculator.html");
 })
+
+app.post("/bmicalculator" , function(req , res){
+    var weight = parseFloat(req.body.weight);
+    var height = parseFloat(req.body.height);
+
+    var bmi = weight / (height *height);
+    res.send("Your BMI is " + bmi );
+});
 
 app.listen(3000 , function(){
     console.log("server has started on port 3000");
