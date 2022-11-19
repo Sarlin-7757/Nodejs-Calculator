5da63121a262b68304abe1d09450c531e3218dd3
Saransh <saranshlingwal923@gmail.com>
2022-11-19 15:58:07 +0530

    some changes

--- calculator.js
+++ calculator.js
@@ -32,7 +32,7 @@ app.post("/bmicalculator" , function(req , res){
     var bmi = weight / (height *height);
     res.send("Your BMI is " + bmi );
 });
-
+// server code --> 3000
 app.listen(3000 , function(){
     console.log("server has started on port 3000");
 });
